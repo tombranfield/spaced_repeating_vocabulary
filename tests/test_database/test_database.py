485a74ac0e81d3aa89299b3323ceeaa965a2cc1e
Thomas Branfield <tbranfield@gmail.com>
2023-07-22 00:17:26 +0100

    Started writing db creation within temporary folder

--- tests/test_database/test_database.py
+++ tests/test_database/test_database.py
@@ -5,18 +5,22 @@ Testing database.py
 # OK pytest.ini isn't working
 import database
 import pytest
+from pathlib import Path
+from tempfile import TemporaryDirectory
 
 
 # A fixture that creates an empty database
 # in a temporary directory for testing purposes
+# Read the cards source code for more
 @pytest.fixture
 def empty_db():
-    temp_db_filename = "/tempdir/"
-    temp_db = Database(temp_db_path)
+    with TemporaryDirectory() as db_dir:
+        db_path = str(Path(db_dir))
+        temp_db = database.Database(db_path)
 
 
 # Test that the database is created
-def test_database_is_created():
+def test_database_is_created(empty_db):
     print("yo")
 
 
