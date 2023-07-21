d5b52457eccd862a68392ae064f6f761d32a699c
Thomas Branfield <tbranfield@gmail.com>
2023-07-21 23:05:43 +0100

    Added database path using Path, and allowed caller to set the location for testing

--- spaced_repeating_vocabulary/database/database.py
+++ spaced_repeating_vocabulary/database/database.py
@@ -4,8 +4,7 @@ It holds the words, their translations, and other related information.
 """
 
 import sqlite3
-import pathlib
-import os.path
+from pathlib import Path
 
 
 class Database:
@@ -14,27 +13,27 @@ class Database:
     words with their corresponding translations and other related 
     information.
     """
-    # Goes in data folder which is up ../../data/
-    # For now I will put it into the same folder as this module
-    DB_PATH = "wordlists.db"
+#    DB_PATH = str(Path("../../data/wordlists.db"))
+    DB_PATH = str(Path("wordlists.db"))
         
-    def __init__(self):
+    def __init__(self, db_path=DB_PATH):
         """Initializes the database."""
-        self._db_filename = self.DB_PATH
+        self._db_path = db_path
         self._create_new_table()
 
-    def connect_and_execute(self, query: str):
+    def _connect_and_execute(self, query: str):
         """
         Connects to the database, executes the supplied query,
         then closes the connection.
         """
-        connection = sqlite3.connect(self._db_filename)
+        connection = sqlite3.connect(self._db_path)
         cursor = connection.cursor()
         cursor.execute(query)
         connection.commit()
         cursor.close()
         connection.close()
 
+    # TODO have an internal create query method?
     def _create_new_table(self):
         """Creates a new database, if it doesn't already exist."""
         create_table_query = """CREATE TABLE master_wordlist (
@@ -50,11 +49,11 @@ class Database:
                                 num_incorrect INTEGER NOT NULL,
                                 is_known INTEGER NOT NULL,
                                 is_review INTEGER NOT NULL);"""
-        self.connect_and_execute(create_table_query)
-        print("SQLite table", self._db_filename, "created.")
+        self._connect_and_execute(create_table_query)
+        print("SQLite table", self._db_path, "created.")
 
 
 if __name__ == "__main__":
     # Quick tests here... don't forget to pytest.
 
-    db = Database()
+    db = Database("test_db.db")
