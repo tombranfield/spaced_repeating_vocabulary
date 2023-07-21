9f9c505ab00a3bfe584baf17c6838688291a197c
Thomas Branfield <tbranfield@gmail.com>
2023-07-21 23:22:21 +0100

    Added test_database in tests/database

---
+++ tests/test_database/test_database.py
@@ -0,0 +1,14 @@
+"""
+Testing database.py
+"""
+
+import database
+
+
+# A fixture that creates an empty database
+# in a temporary directory for testing purposes
+
+
+# Test that the database is created
+def test_database_is_created():
+
