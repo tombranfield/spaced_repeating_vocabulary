9bdb705ab216f3d68b61a3982fef337c3eba7530
Thomas Branfield <tbranfield@gmail.com>
2023-07-22 18:20:11 +0100

    Added Row dataclass to core/

--- spaced_repeating_vocabulary/core/row.py
+++ spaced_repeating_vocabulary/core/row.py
@@ -1,6 +1,23 @@
-# use a dataclass
+"""row.py"""
 
-def Row:
+
+from dataclasses import dataclass
+
+
+@dataclass
+class Row:
     """
-    A class that represents a row of data consisting of a foreign word, its
-    translation, the language it is in
+    A dataclass that represents a row of data consisting of a foreign word, its
+    translation, the language it is in, and the name of the word list it is in.
+    """
+    foreign_word: str
+    translated_word: str
+    language: str
+    word_list_name: str
+
+
+if __name__ == "__main__":
+    my_row = Row("bullig", "cheap", "german", "harry potter und der stein der weisen")
+
+    print(my_row)
+    print(my_row.language)
