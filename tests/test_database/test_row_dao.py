9fa1ebaa19892575dcf49cc8bb9a33153bb44178
Thomas Branfield <tbranfield@gmail.com>
2023-07-23 15:35:36 +0100

    Wrote test_row_dao_insert_row

--- tests/test_database/test_row_dao.py
+++ tests/test_database/test_row_dao.py
@@ -14,18 +14,28 @@ from src.database.row_dao import RowDAO
 def row_of_data():
     row = Row("bullig", "cheap", "german",
               "Harry Potter und der Stein der Weisen")
+    return row
+
 
 @pytest.fixture
 def row_dao_empty_db():
     with TemporaryDirectory() as tmp_dir:
         db_name = "temp_testing_db.db"
         db_path = str(Path(tmp_dir)) + db_name
-        row_dao = RowDao(db_path)
+        row_dao = RowDAO(db_path)
         yield row_dao
 
-def test_row_dao_creates_db(row_dao_empty_db):
-    num_rows_in_db
 
+def test_row_dao_creates_db(row_dao_empty_db):
+    num_rows = row_dao_empty_db.total_rows()
+    assert num_rows == 0
+
+
+def test_row_dao_insert_row(row_dao_empty_db, row_of_data):
+    num_rows_before = row_dao_empty_db.total_rows()
+    row_dao_empty_db.insert_row(row_of_data)
+    num_rows_after = row_dao_empty_db.total_rows()
+    assert num_rows_before == 0 and num_rows_after == 1
 
 
 def test_simple():
