87a7796e450d3c5ee47cc2fb63bf4b7771d2fbfe
Thomas Branfield <tbranfield@gmail.com>
2023-07-23 14:22:45 +0100

    Cleaned docstrings and added Database class properties to row_dao

--- src/database/row_dao.py
+++ src/database/row_dao.py
@@ -1,7 +1,8 @@
 """
 insertion_dao.py
 
-Data Access Object responsible for inserting into the database
+Data Access Object responsible for operations associated with rows
+such as insertion or deletion.
 """
 
 from datetime import datetime
@@ -17,10 +18,10 @@ class RowDAO:
     Acts as an interface to the database in order to perform
     operations on rows such as insertion or deletion.
     """
-    # Get the DB path and table name from the database
-    def __init__(self, db_path=DB_PATH):
-        """Initializes the database."""
-        self.db = database.Database(db_path)
+    def __init__(self, db_path=Database.path):
+        """Initializes the row data access object"""
+        self.db = Database(db_path)
+
 
     def _create_insert_query(self, row):
         """
@@ -28,7 +29,7 @@ class RowDAO:
         database.
         """
         current_datetime = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-        insert_query = ("INSERT INTO " + self.TABLE_NAME + " (list_name," 
+        insert_query = ("INSERT INTO " + Database.table_name + " (list_name," 
                      + "foreign_word, translated_word, language, level, "
                      + "last_learnt_datetime, when_review, num_correct, "
                      + "num_incorrect, is_known, is_review) VALUES( "
@@ -42,8 +43,6 @@ class RowDAO:
                      + "0,0,0,0)")
         return insert_query
 
-        
-    # TODO Equivalent to insert_individual in old program
     def insert_row(self, row):
         """Inserts an individual word into the database"""
         insert_query = self._create_insert_query(row)
@@ -55,5 +54,5 @@ class RowDAO:
 
 if __name__ == "__main__":
 
-    my_dao = InsertionDAO()
-    my_dao.stuff
+    my_dao = RowDAO()
+    my_dao.stuff()
