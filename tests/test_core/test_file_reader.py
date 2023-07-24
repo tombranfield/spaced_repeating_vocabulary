279198a032d4b676cc774f8048f6724bb5edb424
Thomas Branfield <tbranfield@gmail.com>
2023-07-25 00:03:31 +0100

    Wrote some test headers in test_file_reader.py

--- tests/test_core/test_file_reader.py
+++ tests/test_core/test_file_reader.py
@@ -3,3 +3,24 @@ Testing file_reader.py in src/core
 """
 
 from src.core.file_reader import FileReader
+
+
+
+def test_valid_file_classified_as_valid():
+    # return True
+    pass
+
+
+def test_invalid_file_classified_as_invalid():
+    # return True
+    pass
+
+
+def test_file_path_successfully_changed():
+    pass
+
+
+def test_file_data_added_to_database():
+    pass
+
+
