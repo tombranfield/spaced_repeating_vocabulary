7b68f37e09594535f54efbc2574f92d6db018f6e
Thomas Branfield <tbranfield@gmail.com>
2023-07-25 15:31:42 +0100

    Wrote method headers in word_list_dao.py

---
+++ src/database/word_list_dao.py
@@ -0,0 +1,28 @@
+"""
+list_dao.py
+"""
+
+
+class WordListDAO:
+    """
+    Acts as an interface to the database for getting data about a
+    given word list.
+    """
+    def __init__(self):
+        pass
+
+    def word_list_names(self):
+        """Returns the names of all the word lists in the database"""
+        pass
+
+    def language(word_list_name):
+        pass
+
+    def num_words(word_list_name: str):
+        pass
+
+    def num_known_words(word_list_name: str):
+        pass
+
+    def num_unknown_words(word_list_name: str):
+        pass
