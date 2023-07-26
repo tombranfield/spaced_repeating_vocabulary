d7d0708a0df308884a62b4647bb9f90477a5150d
Thomas Branfield <tbranfield@gmail.com>
2023-07-26 17:38:45 +0100

    Removed incorrect type hint from word_list_dao.py

--- src/core/word_list.py
+++ src/core/word_list.py
@@ -44,7 +44,7 @@ class WordList:
     def is_empty(self) -> bool:
         return False if self._word_pairs else True
         
-    def _verify_has_name_and_language(self) -> bool:
+    def _verify_has_name_and_language(self):
         """Returns whether a word list has a name and language"""    
         if not self._name or not self._language:
             raise ValueError("Need to supply a name and language for the word list")
