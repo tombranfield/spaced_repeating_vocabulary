6bea4d45d89b911b863012f2b28fa47b31fce354
Thomas Branfield <tbranfield@gmail.com>
2023-07-26 18:11:53 +0100

    Wrote methods correct(), incorrect(), and is_finished() in quiz_stack.py

--- src/core/quiz_stack.py
+++ src/core/quiz_stack.py
@@ -8,13 +8,34 @@ class QuizStack:
     def __init__(self):
         self.items = [
             "typing_test_quiz",
-            "t_to_f_multiple_quiz",
+            "english_to_foreign_multiple_quiz",
+            "foreign_to_english_multiple_quiz",
             "typing_quiz",
-            "f_to_t_multiple_quiz",
-            "t_to_f_multiple_quiz",
+            "foreign_to_english_multiple_quiz",
+            "english_to_foreign_multiple_quiz",
             "word_defintion",
         ]
 
+    def correct(self):
+        self.items.pop()
+
+    def incorrect_multiple_quiz(self):
+        self.items.append("word_definition")
+
+    def incorrect_typing_quiz(self):
+        self.items.append("word_definition_typing")
+        self.items.append("engish_to_foreign_multiple_quiz")
+
+    def is_finished(self) -> bool:
+        return True if not self.items else False
 
 
-                        
+
+if __name__ == "__main__":
+    learn_stack = QuizStack()
+    for i in range(6):
+        learn_stack.correct()
+    print(learn_stack.is_finished())
+
+    learn_stack.correct()
+    print(learn_stack.is_finished())
