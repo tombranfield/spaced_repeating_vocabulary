1fa1310178f218e62cca7c8b75bb698d6953e067
Thomas Branfield <tbranfield@gmail.com>
2023-07-26 18:04:25 +0100

    Added active_quiz_word and quiz_stack to src/core/

---
+++ src/core/active_quiz_word.py
@@ -0,0 +1,10 @@
+"""active_quiz_word.py"""
+
+
+
+class ActiveQuizWord:
+    """
+    Represents a word that is currently being learned or reviewed
+    """
+    def __init__(self, foreign_word, word_list_name):
+        pass
---
+++ src/core/quiz_stack.py
@@ -0,0 +1,20 @@
+"""quiz_stack.py"""
+
+
+class QuizStack:
+    """
+    Represents the sequence of different tests during a quiz
+    """
+    def __init__(self):
+        self.items = [
+            "typing_test_quiz",
+            "t_to_f_multiple_quiz",
+            "typing_quiz",
+            "f_to_t_multiple_quiz",
+            "t_to_f_multiple_quiz",
+            "word_defintion",
+        ]
+
+
+
+                        
