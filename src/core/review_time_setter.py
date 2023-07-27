7c0efdb45786617a8ff66ca3ef720485143acd4b
Thomas Branfield <tbranfield@gmail.com>
2023-07-27 23:29:09 +0100

    Wrote methods current_time() and next_review_time() in review_time_setter.py

--- src/core/review_time_setter.py
+++ src/core/review_time_setter.py
@@ -8,7 +8,7 @@ import datetime
 
 class ReviewTimeSetter:
     """
-    Responsible for giving words the next time to be reviewed
+    Responsible for determining the next time a word should be reviewed
     """
     def __init__(self, new_level):
         self_new_level = new_level
@@ -30,10 +30,19 @@ class ReviewTimeSetter:
         """
         Returns a string representing the next review time for a word
         """
-        added_hours = self.get_num_hours_from_level()
+        added_hours = self._number_hours_until_review()
+        now = datetime.datetime.now()
+        review_time = now + datetime.timedelta(hours = added_hours)
+        return self._get_datetime_string(review_time)
 
-    def _extra_hours_for_level(self):
-        pass
+    def _number_hours_until_review(self):
+        """Returns how many hours in the future a word should be reviewed"""
+        if self.new_level < 9:
+            return self.level_hours_dict[self.new_level]
+        else:
+            # Time between reviews should be no longer than 180 days
+            return self.level_hours_dict[8]
 
     def _get_datetime_string(self, datetime):
-        pass
+        """Returns a formatted string for a given datetime object"""
+        return datetime.strftime("%d/%m/%Y %H:%M:%S")
