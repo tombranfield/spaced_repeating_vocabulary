8163d48a02e0f1334e8bc4a6c6304834df4b6a4c
Thomas Branfield <tbranfield@gmail.com>
2023-07-28 21:21:07 +0100

    Wrote verify valid level in review_test_setter, and testing current time in test_review_time_setter

--- src/core/review_time_setter.py
+++ src/core/review_time_setter.py
@@ -10,8 +10,9 @@ class ReviewTimeSetter:
     """
     Responsible for determining the next time a word should be reviewed
     """
-    def __init__(self, new_level):
-        self_new_level = new_level
+    def __init__(self, level=1):
+        self._level = level
+        self._verify_valid_level(self._level)
         self.level_hours_dict = {
             1 : 4,
             2 : 24,
@@ -37,7 +38,7 @@ class ReviewTimeSetter:
 
     def _number_hours_until_review(self):
         """Returns how many hours in the future a word should be reviewed"""
-        if self.new_level < 9:
+        if self._level < 9:
             return self.level_hours_dict[self.new_level]
         else:
             # Time between reviews should be no longer than 180 days
@@ -46,3 +47,8 @@ class ReviewTimeSetter:
     def _get_datetime_string(self, datetime):
         """Returns a formatted string for a given datetime object"""
         return datetime.strftime("%d/%m/%Y %H:%M:%S")
+
+    def _verify_valid_level(self, level):
+        """Verifies whether the supplied level is valid"""
+        if level < 1 or not isinstance(level, int):
+            raise ValueError("Entered level must be a positive integer")
--- tests/test_core/test_review_time_setter.py
+++ tests/test_core/test_review_time_setter.py
@@ -9,7 +9,7 @@ import pytest
 from src.core.review_time_setter import ReviewTimeSetter 
 
 
-FAKE_TIME = datetime.datetime(2023, 12, 25, 9, 0, 30)
+MOCK_DATETIME = datetime.datetime(2023, 12, 25, 9, 0, 30)
 
 
 @pytest.fixture
@@ -18,16 +18,22 @@ def datetime_now(monkeypatch):
     class mydatetime:
         @classmethod
         def now(cls):
-            return FAKE_TIME
+            return MOCK_DATETIME
 
     monkeypatch.setattr(datetime, "datetime", mydatetime)
 
 
+@pytest.fixture
+def review_time_setter():
+    return ReviewTimeSetter()
 
 
 def test_patch_datetime(datetime_now):
-    assert datetime.datetime.now() == FAKE_TIME
+    assert datetime.datetime.now() == MOCK_DATETIME
 
+
+def test_current_time(datetime_now, review_time_setter):
+    assert review_time_setter.current_time() == "25/12/2023 09:00:30"
 
 
 def test_returns_correct_review_time_in_desired_format():
