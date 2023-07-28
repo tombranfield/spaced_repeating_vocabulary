c8f86cee8640d673ec1fb6c0a69a8a7465b780fa
Thomas Branfield <tbranfield@gmail.com>
2023-07-28 23:25:26 +0100

    Added test_word_selector.py

---
+++ tests/test_core/test_word_selector.py
@@ -0,0 +1,9 @@
+"""test_word_selector.py"""
+
+
+from src.core.word_selector import WordSelector
+
+
+
+def test_can_initialize_word_selector_successfully():
+    word_selector = WordSelector("My Word List")
