06a672e775b806e4491691c7d1284c3743041699
Thomas Branfield <tbranfield@gmail.com>
2023-07-30 23:13:01 +0100

    Created settings_window.ui and settings_window.py

--- src/gui/main_window.py
+++ src/gui/main_window.py
@@ -15,6 +15,7 @@ class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi(os.path.join(base_dir, "mainwindow.ui"), self)
+        self.setStyleSheet(open(os.path.join(base_dir,"stylesheet.css")).read())
 
         self.about_button.clicked.connect(self.open_about_window)
 
--- src/gui/settings_window.py
+++ src/gui/settings_window.py
@@ -8,11 +8,11 @@ from PyQt5.QtWidgets import QDialog
 base_dir = os.path.dirname(__file__)
 
 
-class AboutWindow(QDialog):
-    """A window that tells the user what the program is and how to use it"""
+class SettingsWindow(QDialog):
+    """A window for viewing and setting the program settings"""
     def __init__(self, parent=None):
         super().__init__(parent)
-        uic.loadUi(os.path.join(base_dir, "about_window.ui"), self)
+        uic.loadUi(os.path.join(base_dir, "settings_window.ui"), self)
         self.setStyleSheet(open("stylesheet.css").read())
 
         self.ok_button.clicked.connect(self.close_window)
