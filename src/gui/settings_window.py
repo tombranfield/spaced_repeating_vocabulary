dbf6a99f480e06e2c6de1e5bdaf8493052cd1715
Thomas Branfield <tbranfield@gmail.com>
2023-07-31 17:23:30 +0100

    Named buttons in settings_window.ui and put into settings_window.py

--- src/gui/settings_window.py
+++ src/gui/settings_window.py
@@ -4,6 +4,9 @@ import sys
 from PyQt5 import uic
 from PyQt5.QtWidgets import QDialog
 
+# TODO
+# from src.core.settings import Settings
+
 
 base_dir = os.path.dirname(__file__)
 
@@ -16,5 +19,27 @@ class SettingsWindow(QDialog):
         self.setStyleSheet(open("stylesheet.css").read())
 
 
+    def num_learn_words_changed(self, s):
+        self.settings.set_max_learn_words(int(s))
+
+
+    def num_review_words_changed(self, s):
+        self.settings.set_max_review_words(int(s))
+
+
     def set_defaults(self):
         pass
+
+
+#    self.settings = Settings()
+
+"""
+    self.set_defaults_button
+    
+    self.is_case_sensitive_cbox
+    self.is_automatic_return_cbox
+
+    self.max_learn_words_box
+    self.max_review_words_box
+"""
+
