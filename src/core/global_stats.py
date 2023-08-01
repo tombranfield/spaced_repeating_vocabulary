18a43c57ee023e52744e7f91eec6cedca76b9cd3
Thomas Branfield <tbranfield@gmail.com>
2023-08-01 18:06:16 +0100

    Wrote total_words_learnt() and total_words() in global_stats.py

--- src/core/global_stats.py
+++ src/core/global_stats.py
@@ -13,18 +13,26 @@ class GlobalStats:
 
     def total_words_learnt(self) -> int:
         """Returns the total number of words learnt from all word lists"""
-        query = ("SELECT COUNT(*) FROM " + Database.table_name 
+#        query = ("SELECT COUNT(*) FROM " + Database.table_name 
+        query = ("SELECT COUNT(*) FROM " + "master_wordlist"
                 + " WHERE is_known = 1")
         result = self.db.result_from_query(query)
-        return result
+        return result[0][0]
 
     def total_words_to_review(self) -> int:
         pass
 
     def total_words(self) -> int:
         """Returns the total number of words in the database"""
-        pass
+        query = "SELECT COUNT(*) FROM " + "master_wordlist"
+        result = self.db.result_from_query(query)
+        return result[0][0]
 
 
 if __name__ == "__main__":
-    global_stats = GlobalStats()
+    
+    global_stats = GlobalStats("harry_potter.db")
+
+    print("Total words:", global_stats.total_words())
+    print("Total words learned:", global_stats.total_words_learnt())
+    print("Total words to review:")
