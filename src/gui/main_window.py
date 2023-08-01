c626e18a64786879f5911abd963ad933b16d1d01
Thomas Branfield <tbranfield@gmail.com>
2023-08-01 23:50:57 +0100

    Wrote setup_stats_update in main_window.py

--- src/gui/main_window.py
+++ src/gui/main_window.py
@@ -1,11 +1,15 @@
+"""main_window.py"""
+
 import os
 import sys
 
 from PyQt5 import QtCore, QtGui, QtWidgets, uic
+from PyQt5.QtCore import QTimer
 from PyQt5.QtWidgets import QDialog
 
 from about_window import AboutWindow
 from settings_window import SettingsWindow
+from src.core.total_stats import TotalStats
 
 
 base_dir = os.path.dirname(__file__)
@@ -17,8 +21,16 @@ class MainWindow(QtWidgets.QMainWindow):
         uic.loadUi(os.path.join(base_dir, "mainwindow.ui"), self)
         self.setStyleSheet(open(os.path.join(base_dir,"stylesheet.css")).read())
 
+        self.setup_stats_update()
+
         self.about_button.clicked.connect(self.open_about_window)
         self.settings_button.clicked.connect(self.open_settings_window)
+        #self.review_all_button.clicked.connect()
+
+
+#    total_learn_label
+#    total_review_label
+
 
     def open_about_window(self):
         dialog = AboutWindow(self)
@@ -28,8 +40,11 @@ class MainWindow(QtWidgets.QMainWindow):
         dialog = SettingsWindow(self)
         dialog.exec_()
 
-
-
+    def setup_stats_update(self):
+        self.timer = QTimer()
+        self.timer.setInterval(1000)
+        self.timer.timeout.connect(self.update_stats_display)
+        self.timer.start()
 
 
 
