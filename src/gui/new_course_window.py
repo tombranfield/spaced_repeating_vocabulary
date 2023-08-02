d7e550e75098273979a8b72edbc786d97513886b
Thomas Branfield <tbranfield@gmail.com>
2023-08-02 23:37:33 +0100

    Added methods to new_course_window.py

--- src/gui/new_course_window.py
+++ src/gui/new_course_window.py
@@ -17,10 +17,35 @@ class NewCourseWindow(QDialog):
         uic.loadUi(os.path.join(base_dir, "new_course_window.ui"), self)
         self.setStyleSheet(open("stylesheet.css").read())
 
+        self.clear_fields_button.clicked.connect(self.clear_fields)
+        self.language_entry.textChanged.connect(self.course_name_changed)
+        self.name_entry.textChanged.connect(self.course_language_changed)
+
+        self.ok_button.clicked.connect(self.submit_inputs)
         self.cancel_button.clicked.connect(self.close_window)
-        self.ok_button.clicked.connect(self.close_window)
-        
+
+
+    def clear_fields(self):
+        """Clears the lineEdit fields of the course name and language"""
+        self.name_entry.setText("")
+        self.language_entry.setText("")
     
     def close_window(self):
+        """Closes the window"""
         self.close()
 
+    def submit_inputs(self):
+        """Submits and stores the course information"""
+        pass
+
+    def check_valid_inputs(self) -> bool:
+        """Checks that the course name and language are valid and new"""
+        # Check there is text in both fields
+        # Check that the supplied name does not already exist
+        # Submit a bool
+
+    def course_name_changed(self):
+        pass
+
+    def course_language_changed(self):
+        pass
