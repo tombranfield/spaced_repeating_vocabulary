84b4896e6e5ab97b05fbe7d0f9bfe1190f417281
Thomas Branfield <tbranfield@gmail.com>
2023-08-03 18:31:16 +0100

    course_names_box in course_chooser_widget now loads a list of courses

--- src/gui/course_chooser_widget.py
+++ src/gui/course_chooser_widget.py
@@ -1,18 +1,30 @@
 """course_chooser.py"""
 
+
 from pathlib import Path
 
 from PyQt5 import QtCore, QtGui, QtWidgets, uic
 from PyQt5.QtCore import QTimer
 from PyQt5.QtWidgets import QDialog, QMainWindow, QWidget
 
+from src.database.courses_dao import CoursesDAO
+
 
 class CourseChooserWidget(QMainWindow):
     def __init__(self):
         super().__init__()
-#        uic.loadUi(os.path.join(base_dir, "course_chooser_widget.ui"), self)
         uic.loadUi(str(Path(__file__).parents[0] / "course_chooser_widget.ui"), self)
         self.setStyleSheet(open("stylesheet.css").read())
+        self.setup_widgets()
+
+    def setup_widgets(self):
+        self.setup_course_names_box()
+
+    def setup_course_names_box(self):
+        courses_dao = CoursesDAO()
+        course_names = courses_dao.courses_list()
+        self.course_names_box.addItems(course_names)
+
 
 
 if __name__ == "__main__":
