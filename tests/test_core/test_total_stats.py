5e4c5de7086b3af60f5b2f5f9a09dbd611296d3e
Thomas Branfield <tbranfield@gmail.com>
2023-08-05 22:52:14 +0100

    Wrote test for total_words_to_learn() for total_stats

--- tests/test_core/test_total_stats.py
+++ tests/test_core/test_total_stats.py
@@ -98,3 +98,8 @@ def test_returns_zero_for_word_list_that_does_not_exist(db_with_learnt_words):
     assert total_stats.total_words_learnt("Non-existent List") == 0
     assert total_stats.total_words_to_review("Non-existent List") == 0
     assert total_stats.total_words("Non-existent List") == 0
+
+
+def test_returns_correct_total_words_to_learn(db_with_learnt_words):
+    total_stats = TotalStats(db_with_learnt_words._db_path)
+    assert total_stats.total_words_to_learn() == 6
