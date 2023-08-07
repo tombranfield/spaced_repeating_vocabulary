418aeb080146061dd5687591a223dcc5cf8345ad
Thomas Branfield <tbranfield@gmail.com>
2023-08-07 22:28:31 +0100

    Renamed active_quiz_word.py to quiz_word.py and wrote some attributes for it

---
+++ src/core/quiz_word.py
@@ -0,0 +1,27 @@
+"""quiz_word.py"""
+
+
+
+class QuizWord:
+    """
+    Represents a word that is currently being learned or reviewed
+    """
+    def __init__(self, id, word_pair: WordPair):
+        self._id = id
+        self._foreign_word = word_pair.foreign_word
+        self._translated_word = word_pair.translated_word
+        self.progress_score = 0
+        self.num_correct = 0
+        self.num_incorrect = 0
+
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def foreign_word(self):
+        return self._foreign_word
+
+    @property
+    def translated_word(self):
+        return self._translated_word
