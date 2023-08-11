d0e5a593200a63e1d902092ecb5186c44bb19f97
Thomas Branfield <tbranfield@gmail.com>
2023-08-11 21:38:32 +0100

    Added setup_next_button()

--- src/gui/quiz_definition.py
+++ src/gui/quiz_definition.py
@@ -19,7 +19,7 @@ class QuizDefinition(QWidget):
 
     send_next = pyqtSignal(int)
         
-    def __init__(self, quiz_word, parent=None):
+    def __init__(self, quiz_word, is_typing=False, parent=None):
         super(QuizDefinition, self).__init__(parent=parent)
         uic.loadUi(str(Path(__file__).parents[0] / "quiz_definition.ui"), self)
         stylesheet_path = str(Path(__file__).parents[0] / "stylesheet.css")
@@ -27,14 +27,33 @@ class QuizDefinition(QWidget):
 
         self.quiz_word = quiz_word
         self.setup_labels()
+        self.setup_next_button()
+
+        self.show_typing_widgets(is_typing)            
 
     def setup_labels(self):
         self.foreign_word_label.setText(self.quiz_word.foreign_word)
         self.translated_word_label.setText(self.quiz_word.translated_word)
         self.translated_word_label.setStyleSheet("font-size: 28px")
         self.instructions_label.setStyleSheet("font-size: 20px")
-        self.send_next.connect(self.parent().next_slot)
 #        self.next_button.clicked.connect(self.send_signal)
+
+    def setup_next_button(self):
+        self.send_next.connect(self.parent().next_slot)
+        self.next_button.setDefault(True)
+        self.next_button.setAutoDefault(True)
+        self.next_button.clicked.connect(self.next_pressed)
 
     def send_signal(self):
         self.send_next.emit(1)
+
+    def show_typing_widgets(self, is_typing):
+        """Shows the instructions and definition entry widgets"""
+        if is_typing:
+            self.instructions_label.show()
+            self.definition_entry.show()
+            self.next_button.hide()
+        else:
+            self.instructions_label.hide()
+            self.definition_entry.hide()
+            self.next_button.show()
