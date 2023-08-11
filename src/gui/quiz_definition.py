856cd26156d37770d1177822afde07c48e92716f
Thomas Branfield <tbranfield@gmail.com>
2023-08-11 21:53:45 +0100

    Added setup_definition_entry and definition_entry_return_pressed in quiz_definition

--- src/gui/quiz_definition.py
+++ src/gui/quiz_definition.py
@@ -19,7 +19,7 @@ class QuizDefinition(QWidget):
 
     send_next = pyqtSignal(int)
         
-    def __init__(self, quiz_word, is_typing=False, parent=None):
+    def __init__(self, quiz_word, is_typing=True, parent=None):
         super(QuizDefinition, self).__init__(parent=parent)
         uic.loadUi(str(Path(__file__).parents[0] / "quiz_definition.ui"), self)
         stylesheet_path = str(Path(__file__).parents[0] / "stylesheet.css")
@@ -28,7 +28,7 @@ class QuizDefinition(QWidget):
         self.quiz_word = quiz_word
         self.setup_labels()
         self.setup_next_button()
-
+        self.setup_definition_entry()
         self.show_typing_widgets(is_typing)            
 
     def setup_labels(self):
@@ -36,16 +36,6 @@ class QuizDefinition(QWidget):
         self.translated_word_label.setText(self.quiz_word.translated_word)
         self.translated_word_label.setStyleSheet("font-size: 28px")
         self.instructions_label.setStyleSheet("font-size: 20px")
-#        self.next_button.clicked.connect(self.send_signal)
-
-    def setup_next_button(self):
-        self.send_next.connect(self.parent().next_slot)
-        self.next_button.setDefault(True)
-        self.next_button.setAutoDefault(True)
-        self.next_button.clicked.connect(self.next_pressed)
-
-    def send_signal(self):
-        self.send_next.emit(1)
 
     def show_typing_widgets(self, is_typing):
         """Shows the instructions and definition entry widgets"""
@@ -57,3 +47,24 @@ class QuizDefinition(QWidget):
             self.instructions_label.hide()
             self.definition_entry.hide()
             self.next_button.show()
+
+    def setup_next_button(self):
+        self.send_next.connect(self.parent().next_slot)
+        self.next_button.clicked.connect(self.send_signal)
+        self.next_button.setDefault(True)
+        self.next_button.setAutoDefault(True)
+
+    def setup_definition_entry(self):
+        self.definition_entry.setPlaceholderText(self.quiz_word.foreign_word)
+        self.definition_entry.returnPressed.connect(
+            self.definition_entry_return_pressed
+        )
+
+    def definition_entry_return_pressed(self):
+        current_text = self.definition_entry.text()
+        if current_text == self.quiz_word.foreign_word:
+            self.send_signal()
+
+    def send_signal(self):
+        self.send_next.emit(1)
+
