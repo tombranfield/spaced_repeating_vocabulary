751670cbae424c2a06dcdb882fc21c21ef30b5fb
Thomas Branfield <tbranfield@gmail.com>
2023-08-15 15:46:06 +0100

    Implemented methods in quiz_stack incl. next_quiz, correct(), incorrect_multiple_quiz() and incorrect_typing_quiz()

--- src/core/quiz_stack.py
+++ src/core/quiz_stack.py
@@ -6,43 +6,42 @@ class QuizStack:
     Represents the sequence of different tests during a quiz
     """
     learn_sequence  = (
-            "typing_test_quiz",
-            "english_to_foreign_multiple_quiz",
-            "foreign_to_english_multiple_quiz",
-            "typing_quiz",
-            "foreign_to_english_multiple_quiz",
-            "english_to_foreign_multiple_quiz",
-            "word_defintion",
+        "typing_test_quiz",
+        "english_to_foreign_multiple_quiz",
+        "foreign_to_english_multiple_quiz",
+        "typing_quiz",
+        "foreign_to_english_multiple_quiz",
+        "english_to_foreign_multiple_quiz",
+        "word_definition",
         )
     review_sequence = (
         "typing_quiz",
     )
+    
     def __init__(self, quiz_type):
-        if quiz_type == "learn":
+        self.quiz_type = quiz_type
+        if self.quiz_type == "learn":
             self.items = list(learn_sequence)
-        elif quiz_type == "review":
+        elif self.quiz_type == "review":
             self.items = list(review_sequence)
+
+    def next_quiz(self):
+        return self.items[-1] 
 
     def correct(self):
         self.items.pop()
 
     def incorrect_multiple_quiz(self):
-        self.items.append("word_definition")
+        if self.quiz_type == "learn":
+            self.items.append("word_definition")
 
     def incorrect_typing_quiz(self):
-        self.items.append("word_definition_typing")
-        self.items.append("engish_to_foreign_multiple_quiz")
+        if self.quiz_type == "learn":
+            self.items.append("word_definition_typing")
+            self.items.append("english_to_foreign_multiple_quiz")
+        elif self.quiz_type == "review":
+            self.items.append("foreign_to_english_multiple_quiz")
+            self.items.append("english_to_foreign_multiple_quiz")
 
     def is_finished(self) -> bool:
         return True if not self.items else False
-
-
-
-if __name__ == "__main__":
-    learn_stack = QuizStack()
-    for i in range(6):
-        learn_stack.correct()
-    print(learn_stack.is_finished())
-
-    learn_stack.correct()
-    print(learn_stack.is_finished())
