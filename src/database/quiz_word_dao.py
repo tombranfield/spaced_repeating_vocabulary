ca789b26ab8cebe041226f96a97be951627ba903
Thomas Branfield <tbranfield@gmail.com>
2023-08-17 14:50:14 +0100

    Wrote set_when_to_review() in quiz_word_dao.py

--- src/database/quiz_word_dao.py
+++ src/database/quiz_word_dao.py
@@ -15,9 +15,8 @@ class QuizWordDAO:
         self.db = Database(db_path)
 
     def set_as_learned(self, quiz_word):
-        # using quiz_word.id
-        # Sets is_known column to 1
-        # increase level by 1
+        self.set_word_as_known(quiz_word)
+        self.increase_level(quiz_word)
         # set learnt datetime
         # set when to review
         pass
@@ -32,6 +31,11 @@ class QuizWordDAO:
         new_num_incorrect = cur_num_incorrect + num_incorrect
         self._update_column_value(quiz_word, "num_incorrect", new_num_incorrect)
 
+    def set_as_learnt(self, quiz_word):
+        self.set_word_as_known(quiz_word)
+        self.increase_level(quiz_word)
+        self.set_when_to_review(quiz_word)
+
     def set_word_as_known(self, quiz_word):
         self._update_column_value(quiz_word, "is_known", 1)
 
@@ -41,11 +45,10 @@ class QuizWordDAO:
         self._update_column_value(quiz_word, "level", new_level)
 
     def set_when_to_review(self, quiz_word):
-        # grabs the current level from the database
-        # Calls ReviewTimeSetter with that level
-        # Calls ReviewTimeSetter's next_review_time method and gets a string
-        # Modifies the when_review column with that datetime
-        pass
+        cur_level = self._get_column_value(quiz_word, "level")
+        review_time_setter = ReviewTimeSetter(cur_level)
+        next_review_time = review_time_setter.next_review_time()
+        self._update_column_value(quiz_word, "when_review", next_review_time)
 
     def _get_column_value(self, quiz_word, column_name):
         query = (
